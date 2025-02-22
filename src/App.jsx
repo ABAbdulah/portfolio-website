ae7b693a293ed44f2f1f2a39a4e3d22b2dc45476
Abdullah Tahir <89691370+ABAbdulah@users.noreply.github.com>
2025-02-23 02:14:00 +0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Analytics } from "@vercel/analytics/react"
-import HomePage from './components/HomePage'
-import MyProjects from './components/myProjects'
 function App() {
   return (
     <div>
